83e8e3fbadd332c5a55f043ffd6d651b6636eb6b
Chennakesavapython <mh6609505@gmail.com>
2025-07-19 17:37:18 +0530

    type

---
+++ basics/demo.py
@@ -0,0 +1,30 @@
+#print("hello")
+
+#import keyword
+#print(keyword.kwlist)
+
+
+#from math import sqrt
+#print(sqrt(16))
+
+#store data
+
+st_name = "Chenna"
+print(type(st_name))
+print(id(st_name))
+st_roll = 17
+print(type(st_roll))
+print(id(st_roll))
+st_1_cga = 8.7
+print(type(st_1_cga))
+print(id(st_1_cga))
+st_2_cga = 8.7
+print(id(st_2_cga))
+
+
+#define complex data
+list_1 = [1,2,3]
+list_2 = [1,2,3]
+
+print(id(list_1))
+print(id(list_2))
