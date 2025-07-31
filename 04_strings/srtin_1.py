f0e5fd6cd2be1d868a191f3cbcdce4f5f6936ade
Chennakesavapython <mh6609505@gmail.com>
2025-07-31 15:12:48 +0530

    string-slicing

---
+++ 04_strings/srtin_1.py
@@ -0,0 +1,16 @@
+text  = "python"
+#slicing forward
+
+print(text[1:4])
+
+#sliicing backward
+
+print(text[1:4:-1]) #empty slicing there is nothing after thaat part
+
+print(text[-1:-4:-1]) #noh
+
+print(text[-4:-6:1]) #empty there is nothing to show
+
+print(text[-4:-6:-1]) #ty
+
+print(text[10])  #it is an indexing   -> string
