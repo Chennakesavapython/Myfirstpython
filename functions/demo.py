8bc7cdbe52b1f1f32704b1273de07eb15e0e60f9
Chennakesavapython <mh6609505@gmail.com>
2025-08-17 22:37:25 +0530

    functions

---
+++ functions/demo.py
@@ -0,0 +1,35 @@
+#without function
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
+a=12
+b=20
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+#define function
+a=100
+b=200
+
+def arithematic():
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+
+#call function --> 100,200
+arithematic()
+
+a=1000
+b=20000
+arithematic()#call the function 1000,20000
+
+
+#functions with parameters
