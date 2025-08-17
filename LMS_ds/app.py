68484a98640b8c0428e6ea378fda0424b182c769
Chennakesavapython <mh6609505@gmail.com>
2025-08-17 18:06:40 +0530

    update_student

---
+++ LMS_ds/app.py
@@ -0,0 +1,106 @@
+#lms project using data structures
+
+#system information
+
+SYSTEM_INFO = ("LMS Student Portal","v1.0","2025","LMS University")
+ADMIN_INFO = ("chennakesavarao89@gmail.com","7989004552","201")
+
+#DISPLAY SYSTEM INFO
+print("="*50)
+print(f"WELCOME TO {SYSTEM_INFO[0]}")  #WELCOME TO LMS SSTUDENT PORTAL  BY USING TUPLES WE DO THIS
+print(f"DEVELOPED BY {SYSTEM_INFO[3]}") #DEVELOPED BY LMS UNIVERSITY
+print("="*50) 
+
+#STORE ALL THE STUDENTS
+#to store details we use dictionaries
+
+Students = {}
+
+#start with menu selection or option selection
+#we use lopping system
+
+while True:
+    print("Choose an option from (1-5): ")
+    print("1.Add student")
+    print("2.Update student")
+    print("3.delete student")
+    print("4.List all students")
+    print("5.exist system")
+
+
+    choice = input("Enter Your Choice: ")
+
+    if choice =="1":
+        print("Performming Operation 1")
+        std_id  = input("enter the student Id : ")
+        #if exists
+        if std_id in Students:
+             print("student Id already exists in the system")
+        else: 
+            std_name = input("enter student name").title()
+            #store multiple scoores
+            Scores = []
+            while True:
+            
+             score_input = input("enter the score or done: ") 
+             #validate input it num or done
+             if score_input == "done":
+                 break
+             if score_input.isdigit():
+                score = int(score_input)
+
+                if 0 <= score <= 100:
+                 Scores.append(score)
+                else:
+                 print("Score should be between 1-100")
+             else:
+                print("Score should be numbers only")    
+
+            # store multiple unique skills
+            skills = set()
+            while True:
+                skill_input = input("enter the skill or type done: ")
+                if skill_input == "done":
+                    break
+                skills.add(skill_input.strip().title())
+
+            #save student details received so d=far
+            Students[std_id] = {
+                "name": std_name,
+                "scores":score,
+                "skills": skills
+            }      
+            print("student added successfully!")
+
+            #for verification
+            print(Students)          
+                  
+                     
+
+
+
+    elif choice =="2":
+        print("Performming Operation 2")  
+        std_id = input("enter the student ID :  ")
+        if std_id in Students:
+            new_name = input("Enter NEW NAME to Update").title()
+            Students[std_id] ["name"] = new_name
+            print("Student Updation is successfully")
+        else:
+            print("student ID doesn't exists")  
+            print(Students)  
+
+
+
+    elif choice == "3":
+         print("Performming Operation 3")
+    elif choice == "4":
+         print("Performming Operation 4")
+    elif choice == "5":
+         print("Performming Operation 5")
+         break
+    else: 
+         print("Invalid Choice")     
+         print("please select from above options")  
+
+
